fd21015bdd1d3f66ad1d13e666def8fbcde3ef2b
URANIUM <165466934+DevURANIUM@users.noreply.github.com>
2024-10-18 02:20:35 +0330

    Update Saveit.py

--- Saveit.py
+++ Saveit.py
@@ -5,34 +5,27 @@ import subprocess
 import time
 from datetime import datetime as dt
 
-# اطلاعات API که باید از my.telegram.org بگیرید
-api_id = '15158919'
-api_hash = '8fd34311e66d16b2a79566353349ad28'
+# API info to get from my.telegram.org
+api_id = 'YOUR_API_ID'
+api_hash = 'YOUR_API_HASH'
 
-# ساخت یک کلاینت تلگرام
 client = TelegramClient('save', api_id, api_hash)
 
-# تابع برای چک کردن و نصب بسته‌های مورد نیاز
 def check_and_install(package, install_command):
     try:
-        # بررسی می‌کنیم که آیا بسته نصب است یا خیر
         subprocess.run([package, '--version'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         print(f"{package} is already installed.")
     except subprocess.CalledProcessError:
-        # اگر نصب نبود، دستور نصب را اجرا می‌کنیم
         print(f"{package} is not installed. Installing now...")
         os.system(install_command)
 
-# چک کردن و نصب mediainfo
 check_and_install('mediainfo', 'apt install mediainfo -y')
-
-# چک کردن و نصب ffmpeg
 check_and_install('ffmpeg', 'apt install ffmpeg -y')
 
 @client.on(events.NewMessage(pattern=r'\.saveit'))
 async def download(event):
     pvpv = event.sender_id
-    saved_messages_chat_id = "me"  # شناسه‌ی پیام‌های ذخیره‌شده (Saved Messages)
+    saved_messages_chat_id = "me"
     inpv = await event.client.send_message(pvpv, "Downloading...")
 
     if event.reply_to_msg_id:
@@ -50,7 +43,6 @@ async def download(event):
     s = dt.now()
     k = time.time()
 
-    # ذخیره‌سازی فایل در پوشه‌ی downloads
     download_path = "downloads/"
     if not os.path.exists(download_path):
         os.makedirs(download_path)
@@ -73,21 +65,16 @@ async def download(event):
     e = dt.now()
     t = int((e - s).seconds * 1000)
 
-    # چک کردن وجود فایل قبل از ارسال
     if os.path.exists(file_name):
-        # ارسال فایل به پیام‌های ذخیره‌شده (Saved Messages)
         await event.client.send_file(saved_messages_chat_id, file_name, caption=f"File saved by {sssender} ")
     else:
         await event.client.send_message(pvpv, "File not found after download.")
 
-    # حذف پیام‌های موقت
     await inpv.delete()
 
 async def main():
-    # شروع کلاینت و وارد شدن به حساب تلگرام
     async with client:
         await client.run_until_disconnected()
 
-# اجرای برنامه
 if __name__ == "__main__":
     asyncio.run(main())
